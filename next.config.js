17406bc1c800290be8bdf0690851533dea2128ab
lukasz.tomczyk <lukasz@cadcamsolutions.pl>
2026-06-15 13:47:50 +0200

    fix: simplify next.config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "img.clerk.com",
-        pathname: "/**",
-      },
+      { protocol: "https", hostname: "res.cloudinary.com", pathname: "/**" },
+      { protocol: "https", hostname: "img.clerk.com", pathname: "/**" },
     ],
   },
 };
